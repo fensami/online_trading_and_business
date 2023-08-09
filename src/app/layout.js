5767842fa610a1b4982b9e69d066868d567ccd6a
Humaion Kobir <humaionkobir341@gmail.com>
2023-08-09 14:38:54 +0600

    create navigation options route

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          This is Navbar
+        </nav>
+        {children}
+        <footer>
+          this is footer
+        </footer>
+      </body>
     </html>
   )
 }
