2b911b1e0bd8a46358d326563e261033734400b9
Humaion Kobir <humaionkobir341@gmail.com>
2023-08-10 16:30:33 +0600

    Complete Navigation Bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -12,9 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>
-          This is Navbar
-        </nav>
+        <Navbar></Navbar>
         {children}
         <footer>
           this is footer
