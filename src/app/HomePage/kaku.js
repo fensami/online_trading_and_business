45f6df5bf58438aa6bc47686896b4c27a0f809c3
fensami <fensami2323@gmail.com>
2023-08-12 21:43:32 +0600

    kaku

---
+++ src/app/HomePage/kaku.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const kaku = () => {
+  return (
+    <div>
+      <h1>kaku</h1>
+      <p>kaku asdfsad 2jkasdkfhkas sdkjflaksjd</p>
+    </div>
+  );
+};
+
+export default kaku;
