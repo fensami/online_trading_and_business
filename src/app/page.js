cb6751f1a6970dedf51ed334a39654e19a75c40f
fensami <fensami2323@gmail.com>
2023-08-13 23:09:13 +0600

    added somthing

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,7 @@ import Banner from "./HomePage/Banner";
 // import Banner from "./HomePage/Banner";
 import CheckTradingSped from "./HomePage/CheckTradingSped";
 import SubLogin from "./HomePage/SubLogin";
-import TradeTypes from "./HomePage/TradeTypes";
 import UpCommingOther from "./HomePage/UpCommingOther";
-import KakuPage from "./HomePage/kaku";
 import Kaku2 from "./kaku2";
 
  const Home = () => {
