9a9a30bcd4227dfa4f6ae6edf1bad5b71db2c582
MD:Rubrl RM <inforubel251@gmail.com>
2023-08-14 15:41:58 +0600

    some add

--- next.config.js
+++ next.config.js
@@ -1,27 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-    reactStrictMode: true,
     images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "lh3.googleusercontent.com",
-            },
-            {
-                protocol: "https",
-                hostname: "i.ibb.co",
-            },
-        ],
-    },
+        protocol: 'https',
+        domains: ['images.unsplash.com']
+    }
 }
-
-=======
-  images: {
-    protocol: 'https',
-    domains: ['images.unsplash.com']
-  }
-}
->>>>>>> 505b3f1b30ea93fc0a14219edf343c3fdf9d0b16
 
 module.exports = nextConfig
