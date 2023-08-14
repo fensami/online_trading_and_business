63f4038ffd7cd12164ff52e59bfb5121e206abc6
fensami <fensami2323@gmail.com>
2023-08-14 21:29:31 +0600

    responsive dashboard

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 const DashboardLayout = ({children}) => {
   return (
-    <div className='flex gap-10 container mx-auto border bg-[#131720]'>
+    <div className='flex gap-10 container mx-auto border bg-[#131720] text-white'>
       <Sidebar></Sidebar>
       {children}
     </div>
