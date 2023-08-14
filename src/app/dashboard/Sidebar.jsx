dc3d8410743a30f5626d683df046335a6535736e
fensami <fensami2323@gmail.com>
2023-08-14 20:02:45 +0600

    added dash board

--- src/app/dashboard/Sidebar.jsx
+++ src/app/dashboard/Sidebar.jsx
@@ -14,68 +14,54 @@ const dashboardLinks  =[
 
 const Sidebar = () => {
   return (
-    <div>
-
-
-
-      <ul>
-        {
-         dashboardLinks.map((path, title) => <li key={path}>
-          <Link href={path} >{title}</Link></li>)
-          
-        }
-        
-      <Link href='/'>home</Link>
-      <Link href='/'>home</Link>
-      <Link href='/'>home</Link>
-      <Link href='/'>home</Link>
-      <Link href='/'>home</Link>
-      </ul>
-
-
-
-
-
-      <div className='flex flex-col bg-slate-500 w-1/6 text-white p-5 gap-5'>
+  <div className='flex my-2 flex-col bg-slate-500 w-1/6 text-white p-5 gap-5'>
 
 
 <p className='border p-3 rounded-xl'><Link href='/dashboard'>Dashboard</Link></p>
 
 <p className='border p-3 rounded-xl'><Link className='flex gap-2 items-center' href='/dashboard/exchange'> <FaExchangeAlt></FaExchangeAlt> exchange</Link></p>
 
-
+<p className='border p-3 rounded-xl'>
 <Link href='/dashboard/prices'>Prices</Link>
+</p>
 
 
-
+<p className='border p-3 rounded-xl'>
 <Link href='/dashboard/wallets'>wallets</Link>
+</p>
 
 
+<p className='border p-3 rounded-xl'>
+  <Link href='/dashboard/promotions'>Promotions</Link>
+</p>
 
-<Link href='/dashboard/promotions'>Promotions</Link>
 
-
-
+<p className='border p-3 rounded-xl'>
 <Link href='/dashboard/activities'>Activities</Link>
+</p>
 
 
-
+<p className='border p-3 rounded-xl'>
 <Link href='/dashboard/notifications'>Notifications</Link>
+</p>
 
 
-
+<p className='border p-3 rounded-xl'>
 <Link href='/dashboard/settings'>Settings</Link>
+</p>
 
 
-
+<p className='border p-3 rounded-xl'>
 <Link href='/dashboard/utilities'>Utilities</Link>
+</p>
 
 
-
+<p className='border p-3 rounded-xl'>
 <Link href='/dashboard/authentication'>Authentication</Link>
+</p>
 
 </div>
-    </div>
+   
   );
 };
 
