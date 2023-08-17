2f3b075e2c6244650d28f396162fd2b7ac72ae7e
fensami <fensami2323@gmail.com>
2023-08-17 21:30:56 +0600

    some updated

--- src/firebase/firebase.auth.js
+++ src/firebase/firebase.auth.js
@@ -1,5 +1,6 @@
 import {GoogleAuthProvider, getAuth} from "firebase/auth"
-import app from "./firebase.config"
+import app from "./firebase.config";
+// import app from "./firebase.config"
 
 const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
