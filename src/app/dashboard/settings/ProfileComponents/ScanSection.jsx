8c7f1ea5d244a56689ce59336963070b6ae892d7
MD:Rubrl RM <inforubel251@gmail.com>
2023-08-18 11:21:04 +0600

    add to scan something

--- src/app/dashboard/settings/ProfileComponents/ScanSection.jsx
+++ src/app/dashboard/settings/ProfileComponents/ScanSection.jsx
@@ -6,6 +6,8 @@ const ScanSection = () => {
         <div >
 <Image width={90} height={90} src={ScanImg} alt=""/>
 <h2 className="text-2xl font-semibold">Scan</h2>
+<p>Enter the six-digit code from the application</p>
+<p>After scanning the image, the app will display a six-digit code that you can enter below</p>
         </div>
     );
 };
