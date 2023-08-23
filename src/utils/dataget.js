72c6b6fff1c810cad2c2c173f0cc31652b744e39
MD:Rubrl RM <inforubel251@gmail.com>
2023-08-23 22:42:41 +0600

    create real-time money streaming chart

--- src/utils/dataget.js
+++ src/utils/dataget.js
@@ -1,9 +1,9 @@
   
-// const dataget = async () => {
-//          const res = await fetch('http://localhost:5000/wallets', {
-//                 cache: "force-cache"
-//          })
-//          return res.json();
-// };
+const dataget = async () => {
+         const res = await fetch('http://localhost:5000/wallets', {
+                cache: "force-cache"
+         })
+         return res.json();
+};
 
-// export default dataget;
+export default dataget;
