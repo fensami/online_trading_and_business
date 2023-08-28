510a070e970704047583f85cf448cef28bf591e0
Mahmud Preyom <preyom5076@gmail.com>
2023-08-28 13:39:03 +0600

    dashboard route set

--- src/app/adminDashboard/adminSidebarLink.js
+++ src/app/adminDashboard/adminSidebarLink.js
@@ -4,11 +4,18 @@ import React from 'react';
 const SidebarLink = () => {
   return (
     <div className='flex gap-5 flex-col'>
-       
-   <div className='border p-3 rounded-xl'><Link href='/adminDashboard/adminHome' className=''> Home</Link></div>
-   <div className='border p-3 rounded-xl'><Link href='/adminDashboard/livePricing' className=''> Live Pricing</Link></div>
-   
-  
+
+      <div className='border p-3 rounded-xl'><Link href='/adminDashboard/adminHome' className=''> Home</Link></div>
+      <div className='border p-3 rounded-xl'><Link href='/adminDashboard/livePricing' className=''> Live Pricing</Link></div>
+      <div className='border p-3 rounded-xl'><Link href='/adminDashboard/farm' className=''>Farm</Link></div>
+      <div className='border p-3 rounded-xl'><Link href='/adminDashboard/swap' className=''>Swap</Link></div>
+      <div className='border p-3 rounded-xl'><Link href='/adminDashboard/liquidity' className=''>Liquidity</Link></div>
+      <div className='border p-3 rounded-xl'><Link href='/adminDashboard/nfts' className=''>NFTs</Link></div>
+      <div className='border p-3 rounded-xl'><Link href='/adminDashboard/profile' className=''>Profile</Link></div>
+      <div className='border p-3 rounded-xl'><Link href='/adminDashboard/vote' className=''>Vote</Link></div>
+      <div className='border p-3 rounded-xl'><Link href='/adminDashboard/authentication' className=''>Authentication</Link></div>
+
+
     </div>
   );
 };
---
+++ src/app/adminDashboard/swap/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Swap = () => {
+    return (
+        <div>
+            Swap
+        </div>
+    );
+};
+
+export default Swap;
