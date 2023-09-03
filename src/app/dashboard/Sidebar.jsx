1362eed7b54fcf69be6f08ce27038ffb79eee8dc
Your Name <you@example.com>
2023-09-03 18:17:16 +0600

    add some design sidebar link and exchange page

--- src/app/dashboard/Sidebar.jsx
+++ src/app/dashboard/Sidebar.jsx
@@ -20,47 +20,38 @@ const dashboardLinks = [
 
 const Sidebar = () => {
   return (
-    <div>
-      <div className="bg-[#131720] w-full h-full">
-        <div className="drawer lg:drawer-open">
-          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content flex flex-col items-center justify-center">
-            {/* Page content here */}
-            <label htmlFor="my-drawer-2" className="lg:hidden">
-              <GiHamburgerMenu className="text-2xl top-0 left-0 fixed"></GiHamburgerMenu>
-              {/* close icon */}
-            </label>
-          </div>
+    <div className="bg-[#131720] h-screen">
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          {/* Page content here */}
+          <label htmlFor="my-drawer-2" className="lg:hidden">
+            <GiHamburgerMenu className="text-2xl top-0 left-0 fixed"></GiHamburgerMenu>
+            {/* close icon */}
+          </label>
+        </div>
 
-          <div className="drawer-side">
-            <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
 
-            <div className="drawer lg:drawer-open">
-              <input
-                id="my-drawer-2"
-                type="checkbox"
-                className="drawer-toggle"
-              />
-              <div className="drawer-content fixed bg-[#212b39] flex flex-col items-center justify-center">
-                {/* Page content here */}
-                <label htmlFor="my-drawer-2" className="lg:hidden">
-                  <GiHamburgerMenu className="text-3xl m-3"></GiHamburgerMenu>
-                  {/* close icon */}
-                </label>
-              </div>
+          <div className="drawer lg:drawer-open">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content fixed bg-[#212b39] flex flex-col items-center justify-center">
+              {/* Page content here */}
+              <label htmlFor="my-drawer-2" className="lg:hidden">
+                <GiHamburgerMenu className="text-3xl m-3"></GiHamburgerMenu>
+                {/* close icon */}
+              </label>
+            </div>
 
-              <div className="drawer-side">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+            <div className="drawer-side">
+              <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
 
-                <div className="my-2 w-52 bg-[#212b39] text-white p-4 gap-5">
-                  <Link
-                    href="/"
-                    className="text-center mt-5 text-2xl font-bold"
-                  >
-                    OTAB
-                  </Link>
-                  <SidebarLink></SidebarLink>
-                </div>
+              <div className="my-2 w-52 bg-[#212b39] text-white pl-4 py-4 gap-5">
+                <Link href="/" className="text-center my-5 text-2xl font-bold">
+                  OTAB
+                </Link>
+                <SidebarLink></SidebarLink>
               </div>
             </div>
           </div>
--- src/app/dashboard/exchange/page.js
+++ src/app/dashboard/exchange/page.js
@@ -5,16 +5,14 @@ import ChartSection from "./chartSection";
 import TablePriceDeep from "./TablePriceDeep";
 
 export const metadata = {
-  title: 'Exchange | OTAB'}
-
+  title: "Exchange | OTAB",
+};
 
 const ExchangePage = () => {
-
   return (
     <div className="m-6 text-gray-300">
-      <header className="
-       w-full grid lg:grid-cols-3 grid-cols-2  gap-4 items-center justify-between lg:justify-around p-4 font-semibold  md:text-center rounded-md shadow-md">
-        <div className=" md:border-r  border-gray-700 pr-20  md:bg-transparent bg-gray-700 py-4 px-2 rounded-sm">
+      <header className="w-full grid lg:grid-cols-3 grid-cols-2 gap-4 items-center justify-between lg:justify-around p-4 font-semibold md:text-center rounded-md shadow-md">
+        <div className="md:border-r border-gray-700 pr-4 lg:pr-20 md:bg-transparent bg-gray-700 py-4 px-2 rounded-sm h-full md:text-left text-center">
           <Dropdown />
           <div className="mt-6">
             <h2 className="text-[18px] font-bold">24h high</h2>
@@ -22,8 +20,9 @@ const ExchangePage = () => {
           </div>
         </div>
 
-        <div className=" md:border-r  border-gray-700 pr-20">
-          <div className="my-6">
+        <div className="md:border-r border-gray-700 pr-4 lg:pr-20 md:bg-transparent bg-gray-700 py-4 px-2 rounded-sm  md:text-left text-center">
+          {/* Added responsive padding for tablets and desktop */}
+          <div className="mb-6">
             <h2 className="text-[18px] font-bold">Price</h2>
             <p className="text-green-500">21,264.95 </p>
           </div>
@@ -33,53 +32,60 @@ const ExchangePage = () => {
           </div>
         </div>
 
-        <div className=" ">
-          <div className="my-6  ">
+        <div className="md:bg-transparent bg-gray-700 py-4 px-2 rounded-sm  md:text-left text-center">
+          {/* Added responsive padding for tablets and desktop */}
+          <div className="my-6">
             <h2 className="text-[18px] font-bold">24h Change</h2>
             <p className="text-green-500">21,264.95 </p>
           </div>
           <div className="">
             <h2 className="text-[18px] font-bold">24h Volume</h2>
-            <p >21,264.95 </p>
+            <p>21,264.95 </p>
           </div>
         </div>
       </header>
       {/* header part end */}
       <main className="my-6 rounded-md shadow-md">
         {/* chart section */}
-        <div className="grid md:flex  gap-6">
-          <div className="md:w-7/12 w-auto bg-[#171f2a] rounded-md shadow-md p-4">
-           <ChartSection/>
+        <div className="grid md:flex gap-6">
+          <div className="md:w-7/12 w-full md:bg-[#171f2a] bg-[#171f2a] rounded-md shadow-md p-4">
+            <ChartSection />
           </div>
-          <div className="md:w-5/12  w-[100%] bg-[#171f2a] rounded-md shadow-md p-4">
+          <div className="md:w-5/12 w-full bg-[#171f2a] rounded-md shadow-md p-4">
             <RoundCharts />
             <div className="text-center">
-              <h3 className="text-1xl font-semibold text-gray-300 mb-4">Total Balance</h3>
+              <h3 className="text-1xl font-semibold text-gray-300 mb-4">
+                Total Balance
+              </h3>
               <h1 className="text-[28px] font-semibold">0.3475948</h1>
               <p className="text-[#10b981]">11,032.24 USD</p>
-              <button className="btn btn-success btn-xs text-white my-2">WithDraw</button>
+              <button className="btn btn-success btn-xs text-white my-2">
+                Withdraw
+              </button>
             </div>
           </div>
         </div>
         {/* table section */}
-        <div className="grid md:flex  gap-6 mt-6">
-          <div className="md:w-7/12 bg-[#171f2a] rounded-md shadow-md p-4">
-            <div className=" mb-4 md:flex justify-between items-center">
+        <div className="grid md:flex gap-6  mt-8">
+          <div className="md:w-7/12 w-full bg-[#171f2a] rounded-md shadow-md p-4">
+            <div className="mb-4 md:flex justify-between items-center">
               <span>
-                <h1 className="md:text-[28px] text-[20px] text-center font-semibold">Market trades</h1>
+                <h1 className="md:text-[28px] text-[20px] text-center font-semibold">
+                  Market trades
+                </h1>
               </span>
-              {/* <span className="flex gap-4 justify-center mt-2">
-                <button className="btn btn-success btn-sm text-white ">Deep Chart</button>
-                <button className="btn btn-success btn-sm text-white btn-outline">Price Chart</button>
-              </span> */}
             </div>
             <TablePriceDeep />
           </div>
-          <div className="md:w-5/12 bg-[#171f2a] rounded-md shadow-md p-4">
-            <div className=" grid grid-cols-2 gap-4">
-              <button className="btn btn-success btn-sm text-white ">Buy</button>
-              <button className="btn btn-success btn-sm text-white btn-outline">Sell</button>
-              <button className="btn btn-success btn-sm text-white btn-outline">Exchange</button>
+          <div className="md:w-5/12 w-full bg-[#171f2a] rounded-md shadow-md p-4">
+            <div className="grid grid-cols-2 gap-4">
+              <button className="btn btn-success btn-sm text-white">Buy</button>
+              <button className="btn btn-success btn-sm text-white btn-outline">
+                Sell
+              </button>
+              <button className="btn btn-success btn-sm text-white btn-outline">
+                Exchange
+              </button>
             </div>
             <h2 className="text-xl font-semibold mt-6 mb-2">Coin</h2>
             <div className="rounded-md px-6 py-1 bg-black">
@@ -94,11 +100,13 @@ const ExchangePage = () => {
               <span>$1342.00</span>
             </span>
             <div className="text-center">
-              <button className="btn btn-success btn-xs text-white my-2">Buy Bitcoin</button>
+              <button className="btn btn-success btn-xs text-white my-2">
+                Buy Bitcoin
+              </button>
             </div>
           </div>
         </div>
-      </main >
+      </main>
     </div>
   );
 };
