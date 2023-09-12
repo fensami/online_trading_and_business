0a2f466d826c98aa35fdad9deb3f250a5c8147ed
Your Name <you@example.com>
2023-09-12 11:50:48 +0600

    some add

--- src/app/dashboard/settings/ProfileComponents/ScanSection.jsx
+++ src/app/dashboard/settings/ProfileComponents/ScanSection.jsx
@@ -1,15 +1,17 @@
-import Image from "next/image";
-import ScanImg from "../../../../../public/download-removebg-preview.png"
+import QRCodeGenerator from "./QRCodeGenerator";
 
 const ScanSection = () => {
-    return (
-        <div >
-<Image width={90} height={90} src={ScanImg} alt=""/>
-<h2 className="text-2xl font-semibold">Scan</h2>
-<p>Enter the six-digit code from the application</p>
-<p>After scanning the image, the app will display a six-digit code that you can enter below</p>
-        </div>
-    );
+  return (
+    <div>
+      <QRCodeGenerator />
+      <h2 className="text-2xl font-semibold">Scan</h2>
+      <p>Enter the six-digit code from the application</p>
+      <p>
+        After scanning the image, the app will display a six-digit code that you
+        can enter below
+      </p>
+    </div>
+  );
 };
 
 export default ScanSection;
